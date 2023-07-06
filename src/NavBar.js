66374754d3ad803bf158e691932cbb24cf4ac1bf
Ellie Bridges <elliebridges@Ellies-MacBook-Pro.local>
2023-07-06 14:02:07 -0700

    foundation for the website - basic navbar, creating layout component

---
+++ src/NavBar.js
@@ -0,0 +1,15 @@
+
+
+export default function NavBar() {
+    return(
+        <nav class="pa3 pa4-ns">
+        <a class="link dim black b f6 f5-ns dib mr3" href="#" title="Home">We Went Exploring</a>
+        <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="Home">Home</a>
+        <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="About">About</a>
+        <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="Store">Galleries</a>
+        <a class="link dim gray    f6 f5-ns dib mr3"href="#" title="Contact">Our Journey</a>
+        <a class="link dim gray    f6 f5-ns dib" href="#" title="Contact">Reach Out</a>
+      </nav>
+         
+    )
+}
